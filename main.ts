274eeba5905f26414c57f5f5ba1ac1bcbff2574a
Alexandre Dumont <adumont@users.noreply.github.com>
2021-01-20 00:40:48 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,61 @@
+function createLevel () {
+    if (Level == 0) {
+        scene.setBackgroundColor(9)
+        tiles.setTilemap(tiles.createTilemap(hex`1000100000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000000020000000000000200000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, [myTiles.transparency16,myTiles.tile1,myTiles.tile2], TileScale.Sixteen))
+        createPlayer()
+        createMonkeys()
+    }
+}
+function createMonkeys () {
+	
+}
+function createPlayer () {
+    mySprite = sprites.create(img`
+        . . . . . . f f f f . . . . . . 
+        . . . . f f f 2 2 f f f . . . . 
+        . . . f f f 2 2 2 2 f f f . . . 
+        . . f f f e e e e e e f f f . . 
+        . . f f e 2 2 2 2 2 2 e e f . . 
+        . . f e 2 f f f f f f 2 e f . . 
+        . . f f f f e e e e f f f f . . 
+        . f f e f b f 4 4 f b f e f f . 
+        . f e e 4 1 f d d f 1 4 e e f . 
+        . . f e e d d d d d d e e f . . 
+        . . . f e e 4 4 4 4 e e f . . . 
+        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f . . f f . . . . . 
+        `, SpriteKind.Player)
+    scene.cameraFollowSprite(mySprite)
+    tiles.placeOnRandomTile(mySprite, myTiles.tile1)
+    for (let value of tiles.getTilesByType(myTiles.tile1)) {
+        tiles.setTileAt(value, myTiles.transparency16)
+    }
+    controller.moveSprite(mySprite, 100, 0)
+    mySprite.ay = 300
+}
+let mySprite: Sprite = null
+let Level = 0
+Level = 0
+info.setScore(0)
+info.setLife(4)
+createLevel()
